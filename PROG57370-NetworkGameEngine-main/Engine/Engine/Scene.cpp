f7aaf4d26c88e8a247fa9d6ed382ee55ca7ac34d
Lia Fisher <willfisher1007@hotmail.ca>
2024-04-06 16:05:37 -0400

    Scene and SceneManager

--- PROG57370-NetworkGameEngine-main/Engine/Engine/Scene.cpp
+++ PROG57370-NetworkGameEngine-main/Engine/Engine/Scene.cpp
@@ -3,6 +3,7 @@
 #include "Entity.h"
 #include "AssetManager.h"
 #include "NetworkServer.h"
+#include "NetworkRPC.h"
 
 void Scene::Serialize(RakNet::BitStream& bitStream) const
 {
@@ -315,4 +316,23 @@ bool Scene::RemoveEntity(STRCODE entityId)
 		}
 	}
 	return false;
+}
+
+void Scene::InvokeRPC(RakNet::BitStream& bitStream)
+{
+	STRCODE entityID;
+	bitStream.Read(entityID);
+
+	for (const auto entity : entities)
+	{
+		if (entity->GetUid() == entityID)
+		{
+			STRCODE componentID;
+			bitStream.Read(componentID);
+			NetworkRPC* networkRPC = (NetworkRPC*)entity->GetComponentByUiD(componentID);
+			ASSERT(networkRPC != nullptr, "Component is not a NetworkRPC");
+			networkRPC->InvokeRPC(bitStream);
+			break;
+		}
+	}
 }
